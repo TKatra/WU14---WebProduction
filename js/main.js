264a5042955ff32194dd8ae4999410bb5a71c72a
TKatra <tobiaskatra@hotmail.com>
2015-02-03 20:52:57 +0100

    Made a script that manages the margin on the main-content class so the content on the site won't get covered by the footer

--- js/main.js
+++ js/main.js
@@ -1,4 +1,18 @@
 $(function ()
 {
+	function siteStartup()
+	{
+		$(".trial").remove();
+		$(".main-content").css("margin-bottom", $("footer").height() + 15);
+		$( ".main-content" ).prepend( "<div class='trial'>" + $( window ).width() + " x " + $(window).height() + "<br/>Height of footer:" + $("footer").height() + "<br/>Margin on main-content: "+ parseInt($(".main-content").css("margin-bottom")) +" </div>" );
+	}
+	$( window ).resize(function()
+	{
+		$(".trial").remove();
+		$(".main-content").css("margin-bottom", $("footer").height() + 15);
+		$( ".main-content" ).prepend( "<div class='trial'>" + $( window ).width() + " x " + $(window).height() + "<br/>Height of footer:" + $("footer").height() + "<br/>Margin on main-content: "+ parseInt($(".main-content").css("margin-bottom")) +" </div>" );
+
+	});
 	
+	siteStartup();
 });

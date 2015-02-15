71ddd964057ca39303668516b138811a16d01fe1
TKatra <tobiaskatra@hotmail.com>
2015-02-15 18:44:40 +0100

    started building the main.php file

--- php/main.php
+++ php/main.php
@@ -1,3 +1,18 @@
 <?php
+include_once("../libs/nodebite-swiss-army-oop.php");
 
+$connectInfo =array(
+  "host" => "127.0.0.1",
+  "dbname" => "wu14oop2",
+  "username" => "root",
+  "password" => "mysql"
+  );
+
+$request = $_REQUEST;
+$PDOHelper = new PDOHelper($connectInfo["host"], $connectInfo["dbname"], $connectInfo["username"], $connectInfo["password"]);
+
+if($request["commandLine"] == "createNewAccount")
+{
+	
+}
 ?>

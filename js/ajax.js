28d89cf5e935dbe16d8453d4128beb4fd26ef4a6
TKatra <tobiaskatra@hotmail.com>
2015-02-16 15:56:48 +0100

    made ajax commands for adding a new admin and logging in

--- js/ajax.js
+++ js/ajax.js
@@ -11,3 +11,38 @@ function contactPHP(request, successFunction)
 		}
 	});
 }
+
+function createNewAdmin(request)
+{
+	$.ajax({
+		url:"php/main.php",
+		dataType: "json",
+		data: request,
+		success:function(data)
+		{
+			newPage("log-in");
+		},
+		error:function(data)
+		{
+			console.log("AJAX ERROR: ", data.responseText);
+		}
+	});
+}
+
+function logIn(request)
+{
+	$.ajax({
+		url:"php/main.php",
+		dataType: "json",
+		data: request,
+		success:function(data)
+		{
+			console.log("AJAX RETURN DATA: ", data);
+			// newPage("home");
+		},
+		error:function(data)
+		{
+			console.log("AJAX ERROR: ", data.responseText);
+		}
+	});
+}

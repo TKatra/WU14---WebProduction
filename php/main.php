335b90d629ee199ad9fccd3b5d7b426ac2ffd6a2
TKatra <tobiaskatra@hotmail.com>
2015-02-16 16:09:58 +0100

    made the command for creating new admins work, and I'm now working on the log in and log out functions

--- php/main.php
+++ php/main.php
@@ -1,18 +1,51 @@
 <?php
 include_once("../libs/nodebite-swiss-army-oop.php");
 
+session_start();
+
 $connectInfo =array(
   "host" => "127.0.0.1",
-  "dbname" => "wu14oop2",
+  "dbname" => "WU14WebProduction",
   "username" => "root",
   "password" => "mysql"
   );
 
 $request = $_REQUEST;
 $PDOHelper = new PDOHelper($connectInfo["host"], $connectInfo["dbname"], $connectInfo["username"], $connectInfo["password"]);
+$result = array();
+
 
 if($request["commandLine"] == "createNewAccount")
 {
-	
+	$prepareValues = array(
+		"firstName" => $request["firstName"],
+		"lastName" => $request["lastName"],
+		"email" => $request["email"],
+		"password" => sha1($request["password"])
+		);
+
+	$PDOHelper->query(
+	"INSERT INTO Admins (firstName, lastName, email, `password`)
+	VALUES (:firstName, :lastName, :email, :password);",
+	$prepareValues
+	);
+
+	echo(json_encode($result));
+	exit();
+}
+else if($request["commandLine"] == "logIn")
+{
+	$email = $request["email"];
+	$password = sha1($request["password"]);
+
+	//HILFE
+
+	echo(json_encode($result));
+	exit();
+}
+else if($request["commandLine"] == "logOut")
+{
+	echo(json_encode($result));
+	exit();
 }
 ?>

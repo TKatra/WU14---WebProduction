775b02f61de368922468af58f43d65fe60479c31
TKatra <tobiaskatra@hotmail.com>
2015-02-17 16:02:45 +0100

    made a function for the keyUp event on the page-title element in the add page form

--- js/eventListeners.js
+++ js/eventListeners.js
@@ -13,3 +13,15 @@ function onWindowResize()
 	setMainContentMargin();
 	buildDebugWindow();
 }
+
+function pageTitleKeyUp()
+{
+	var form = $(this).parents("form");
+	if (form.find("input[name=manuallyEditURL]:checked").length < 1)
+	{
+		var pageTitle = $(this).val();
+		var pageURL = toUrl(pageTitle);
+
+		$(this).parents("form").find("input[name=pageURL]").val(pageURL);
+	}
+}
